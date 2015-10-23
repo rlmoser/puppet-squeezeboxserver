d32d4ab1e2291c613605b9ff07780f391e1d794a
Robert Moser II <rlmoser@gmail.com>
2015-10-23 11:02:28 -0700

    Consolidate ensure variables for dirs/files

--- manifests/install/vcs.pp
+++ manifests/install/vcs.pp
@@ -1,33 +1,28 @@
 class squeezeboxserver::install::vcs {
   # TODO: make these parameters
-  $install_destination = '/opt/slimserver'
-  $install_source      = 'https://github.com/Logitech/slimserver.git'
-  $install_revision    = 'public/7.9'
-  $install_vcsprovider = 'git'
-  $install_ensure      = 'present'
-  $service_bin         = "${install_destination}/server.pl"
-  $service_charset     = 'utf8'
-  $service_options     = ''
-  $service_file        = '/etc/systemd/system/squeezeboxserver.service'
-  $service_filemode    = '0644'
-  $service_template    = 'puppet:///squeezeboxserver/squeezeboxserver.service.erb'
-  $user_name           = 'squeezeboxserver'
-  $user_ensure         = 'present'
-  $user_comment        = 'Logitech Media Server user'
-  $conf_dir            = '/etc/squeezeboxserver'
-  $conf_dir_ensure     = 'directory'
-  $conf_files_ensure   = 'file'
-  $lib_dir             = '/var/lib/squeezeboxserver'
-  $lib_dir_ensure      = 'directory'
-  $data_dir            = "${lib_dir}/cache"
-  $data_dir_ensure     = 'directory'
-  $log_dir             = '/var/log/squeezeboxserver'
-  $log_dir_ensure      = 'directory'
-  $plugin_dir          = "${lib_dir}/Plugins"
-  $plugin_dir_ensure   = 'directory'
-  $pref_dir            = "${lib_dir}/prefs"
-  $pref_dir_ensure     = 'directory'
-  $use_systemd         = true
+  $install_destination  = '/opt/slimserver'
+  $install_source       = 'https://github.com/Logitech/slimserver.git'
+  $install_revision     = 'public/7.9'
+  $install_vcsprovider  = 'git'
+  $install_ensure       = 'present'
+  $service_bin          = "${install_destination}/server.pl"
+  $service_charset      = 'utf8'
+  $service_options      = ''
+  $service_file         = '/etc/systemd/system/squeezeboxserver.service'
+  $service_filemode     = '0644'
+  $service_template     = 'puppet:///squeezeboxserver/squeezeboxserver.service.erb'
+  $user_name            = 'squeezeboxserver'
+  $user_ensure          = 'present'
+  $user_comment         = 'Logitech Media Server user'
+  $conf_dir             = '/etc/squeezeboxserver'
+  $install_dirs_ensure  = 'directory'
+  $install_files_ensure = 'file'
+  $lib_dir              = '/var/lib/squeezeboxserver'
+  $data_dir             = "${lib_dir}/cache"
+  $log_dir              = '/var/log/squeezeboxserver'
+  $plugin_dir           = "${lib_dir}/Plugins"
+  $pref_dir             = "${lib_dir}/prefs"
+  $custom_perl          = ''
   
   # TODO: logic for ensure: absent
 
@@ -55,7 +50,7 @@ class squeezeboxserver::install::vcs {
   # TODO: SELinux setup for directories/files
 
   file {$conf_dir:
-    ensure  => $conf_dir_ensure,
+    ensure  => $install_dirs_ensure,
     # possibly use root:root
     owner   => $user_name,
     group   => $user_name,
@@ -63,42 +58,42 @@ class squeezeboxserver::install::vcs {
   }
 
   file {$lib_dir:
-    ensure  => $lib_dir_ensure,
+    ensure  => $install_dirs_ensure,
     owner   => $user_name,
     group   => $user_name,
     mode    => '0755',
   }
 
   file {$data_dir:
-    ensure  => $data_dir_ensure,
+    ensure  => $install_dirs_ensure,
     owner   => $user_name,
     group   => $user_name,
     mode    => '0755',
   }
 
   file {$log_dir:
-    ensure  => $log_dir_ensure,
+    ensure  => $install_dirs_ensure,
     owner   => $user_name,
     group   => $user_name,
     mode    => '0755',
   }
 
   file {$plugin_dir:
-    ensure  => $plugin_dir_ensure,
+    ensure  => $install_dirs_ensure,
     owner   => $user_name,
     group   => $user_name,
     mode    => '0755',
   }
 
   file {$pref_dir:
-    ensure  => $pref_dir_ensure,
+    ensure  => $install_dirs_ensure,
     owner   => $user_name,
     group   => $user_name,
     mode    => '0755',
     }
 
   file {"${conf_dir}/convert.conf":
-    ensure  => $conf_files_ensure,
+    ensure  => $install_files_ensure,
     owner   => $user_name,
     group   => $user_name,
     mode    => '0644',
@@ -107,7 +102,7 @@ class squeezeboxserver::install::vcs {
   }
   
   file {"${conf_dir}/modules.conf":
-    ensure  => $conf_files_ensure,
+    ensure  => $install_files_ensure,
     owner   => $user_name,
     group   => $user_name,
     mode    => '0644',
@@ -116,7 +111,7 @@ class squeezeboxserver::install::vcs {
   }
 
   file {"${conf_dir}/types.conf":
-    ensure  => $conf_files_ensure,
+    ensure  => $install_files_ensure,
     owner   => $user_name,
     group   => $user_name,
     mode    => '0644',
@@ -125,7 +120,7 @@ class squeezeboxserver::install::vcs {
   }
 
   file {$service_file:
-    ensure  => $conf_files_ensure,
+    ensure  => $install_files_ensure,
     owner   => root,
     group   => root,
     mode    => $service_filemode,
